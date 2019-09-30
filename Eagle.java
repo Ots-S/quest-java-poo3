45ac6b866eb6c669b50fa7f048d34ec1253378ae
Sebastien Ots <ots.sebastien@gmail.com>
2019-09-30 17:58:24 +0200

    ajout fly

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,35 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println(this.getName() + " takes off in the sky.");
+    }
+
+    @Override
+    public int ascend(int meters) {
+        this.altitude = getAltitude() + meters;
+        System.out.println(this.getName() + " flies upward, altitude : " + this.getAltitude());
+        return this.altitude;
+    } 
+
+    @Override 
+    public int descend(int meters) {
+        this.altitude = getAltitude() - meters;
+        System.out.println(this.getName() + " flies downward, alitude : " + this.getAltitude());
+        return this.altitude;
+    }
+
+    @Override
+    public void land() {
+        if (this.altitude <= 9) {
+            System.out.println(this.getName() + " lands on the ground.");
+        }
+        else {
+        System.out.println(this.getName() + " is too high to land.");
+        }
+        
     }
 }
